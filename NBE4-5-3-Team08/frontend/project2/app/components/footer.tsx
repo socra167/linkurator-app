ecdbd211734c7865dd8ad7ec0e11a568ddf8d0d9
kozae00 <rhwogus777@gmail.com>
2025-04-04 12:13:25 +0900

    delete file

--- NBE4-5-3-Team08/frontend/project2/app/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 text-center py-4">
-      <p className="text-sm text-gray-500">
-        © {new Date().getFullYear()} Linkurator. All rights reserved.
-      </p>
-    </footer>
-  );
-}
