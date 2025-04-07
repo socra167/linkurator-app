b93622c053390f3d56599bde98f6a819f78d98af
yunjuKimm <yunju.tech@gmail.com>
2025-04-07 17:36:40 +0900

    feat: PlaylistLike엔티티 Kotlin 변환, Java deprecated 처리

--- backend/project2/src/main/java/com/team8/project2/domain/playlist/entity/PlaylistLike.java
+++ backend/project2/src/main/java/com/team8/project2/domain/playlist/entity/PlaylistLike.java
@@ -8,6 +8,13 @@ import lombok.Setter;
 
 import java.io.Serializable;
 
+/**
+ * 플레이리스트 좋아요(PlaylistLike) 엔티티 클래스입니다.
+ * 사용자가 플레이리스트에 좋아요를 누른 정보를 저장합니다.
+ * @deprecated Kotlin으로 마이그레이션됨. PlaylistLike.kt
+ */
+
+@Deprecated
 @Entity
 @Getter
 @Setter
