859e989d9a815d064e3ac29bea839bfc248e363c
kozae00 <rhwogus777@gmail.com>
2025-04-07 15:09:38 +0900

    feat: Comment.kt

--- backend/project2/src/main/java/com/team8/project2/domain/comment/entity/Comment.kt
+++ backend/project2/src/main/java/com/team8/project2/domain/comment/entity/Comment.kt
@@ -14,75 +14,70 @@ import java.time.LocalDateTime
  * 각 댓글은 특정 큐레이션(Curation)과 연관되며, 작성자 ID 및 내용 정보를 포함합니다.
  */
 @Entity
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@EntityListeners(
-    AuditingEntityListener::class
-)
-class Comment {
-    /**
-     * 댓글의 고유 ID (자동 생성)
-     */
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private var id: Long? = null
+@EntityListeners(AuditingEntityListener::class)
+class Comment(
 
     /**
      * 댓글 작성자의 사용자 ID
      */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "memberId", nullable = false)
-    private var author: Member? = null
+    val author: Member,
 
     /**
      * 댓글이 속한 큐레이션
      */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "curation_id", nullable = false)
-    private var curation: Curation? = null
-
-    @OneToMany(mappedBy = "comment", orphanRemoval = true)
-    private val replyComments: MutableList<ReplyComment> = ArrayList()
+    val curation: Curation,
 
     /**
      * 댓글 내용 (텍스트 형태, 필수값)
      */
     @Column(nullable = false, columnDefinition = "TEXT")
-    private var content: String? = null
+    var content: String
+
+) {
+    /**
+     * 댓글의 고유 ID (자동 생성)
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+
+    /**
+     * 댓글에 달린 답글 목록
+     */
+    @OneToMany(mappedBy = "comment", orphanRemoval = true)
+    val replyComments: MutableList<ReplyComment> = mutableListOf()
 
     /**
      * 댓글 생성 시간 (수정 불가)
      */
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    @Setter(AccessLevel.PRIVATE)
-    private var createdAt: LocalDateTime? = null
+    var createdAt: LocalDateTime? = null
+        private set
 
     /**
      * 댓글 수정 시간
      */
     @LastModifiedDate
     @Column(nullable = false)
-    @Setter(AccessLevel.PRIVATE)
-    private var modifiedAt: LocalDateTime? = null
+    var modifiedAt: LocalDateTime? = null
+        private set
 
     fun addReplyComment(replyComment: ReplyComment) {
-        replyComments.add(replyComment)
+        this.replyComments.add(replyComment)
     }
 
-    val authorName: String
-        get() = author!!.username
+    fun getAuthorName(): String = author.username
 
-    val authorId: Long
-        get() = author!!.id
+    fun getAuthorId(): Long = author.id
 
-    val authorImgUrl: String
-        get() = author!!.profileImage
+    fun getAuthorImgUrl(): String = author.profileImage
 
-    fun updateContent(content: String?) {
+    fun updateContent(content: String) {
         this.content = content
     }
 }
-
