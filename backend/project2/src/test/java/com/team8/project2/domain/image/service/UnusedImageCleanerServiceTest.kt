49b81ad2bd0e91c96b1ee12a5f94cf6e71840e8d
socra167 <socra167@gmail.com>
2025-04-07 16:09:47 +0900

    refactor: `UnusedImageCleanerServiceTest` Kotlin 전환

--- backend/project2/src/test/java/com/team8/project2/domain/image/service/UnusedImageCleanerServiceTest.kt
+++ backend/project2/src/test/java/com/team8/project2/domain/image/service/UnusedImageCleanerServiceTest.kt
@@ -1,44 +1,47 @@
-package com.team8.project2.domain.image.service;
+package com.team8.project2.domain.image.service
 
-import static org.mockito.Mockito.*;
+import com.team8.project2.domain.image.entity.CurationImage
+import com.team8.project2.domain.image.repository.CurationImageRepository
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers
+import org.mockito.kotlin.any
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.junit.jupiter.MockitoExtension
+import java.time.LocalDateTime
+import java.util.List
 
-import java.time.LocalDateTime;
-import java.util.List;
+@ExtendWith(MockitoExtension::class)
+class UnusedImageCleanerServiceTest {
+    @InjectMocks
+    private val unusedImageCleanerService: UnusedImageCleanerService? = null
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+    @Mock
+    private val curationImageRepository: CurationImageRepository? = null
 
-import com.team8.project2.domain.image.entity.CurationImage;
-import com.team8.project2.domain.image.repository.CurationImageRepository;
+    @Mock
+    private val s3Uploader: S3Uploader? = null
 
-@ExtendWith(MockitoExtension.class)
-public class UnusedImageCleanerServiceTest {
+    @Test
+    @DisplayName("이미지 삭제 스케줄러에서 삭제 작업이 수행된다")
+    fun testCleanUnusedImages() {
+        val imageName = "imageName.png"
 
-	@InjectMocks
-	private UnusedImageCleanerService unusedImageCleanerService;
+        `when`(curationImageRepository!!.findUnusedImages(any<LocalDateTime>()))
+            .thenReturn(List.of(CurationImage(imageName)))
 
-	@Mock
-	private CurationImageRepository curationImageRepository;
+        // when: cleanUnusedImages 메서드 호출
+        unusedImageCleanerService!!.cleanUnusedImages()
 
-	@Mock
-	private S3Uploader s3Uploader;
-
-	@Test
-	@DisplayName("이미지 삭제 스케줄러에서 삭제 작업이 수행된다")
-	public void testCleanUnusedImages() {
-		var imageName = "imageName.png";
-		when(curationImageRepository.findUnusedImages(any(LocalDateTime.class)))
-			.thenReturn(List.of(new CurationImage(imageName)));
-
-		// when: cleanUnusedImages 메서드 호출
-		unusedImageCleanerService.cleanUnusedImages();
-
-		// then: deleteUnusedImage 메서드가 호출되었는지 확인
-		verify(curationImageRepository, times(1)).delete(any(CurationImage.class));
-		verify(s3Uploader, times(1)).deleteFile(imageName);
-	}
+        // then: deleteUnusedImage 메서드가 호출되었는지 확인
+        verify(curationImageRepository, times(1)).delete(
+            ArgumentMatchers.any(
+                CurationImage::class.java
+            )
+        )
+        verify(s3Uploader, times(1))!!.deleteFile(imageName)
+    }
 }
