f2d80783dd9d01896a5d97e3b989d22c8631dcd4
kozae00 <rhwogus777@gmail.com>
2025-04-08 11:07:42 +0900

    feat: LinkResDTO.kt

---
+++ backend/project2/src/main/java/com/team8/project2/domain/link/dto/LinkResDTO.kt
@@ -0,0 +1,23 @@
+package com.team8.project2.domain.link.dto
+
+
+/**
+ * 링크 응답 DTO 클래스입니다.
+ * 클라이언트에 전달할 링크 데이터를 변환하여 제공합니다.
+ */
+data class LinkResDTO(
+    /**
+     * 링크 URL
+     * 링크 TITLE
+     * 링크 DESCRIPTION
+     * 링크 IMAGE
+     */
+    val url: String,
+    val title: String,
+    val description: String,
+    val image: String,
+    val click: Int,
+    val linkId: Long
+) {
+
+}
