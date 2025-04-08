d6c1350d410d534119aeb378d66919e0d0bebb9e
socra167 <socra167@gmail.com>
2025-04-08 11:13:11 +0900

    refactor: `TagResDto` Kotlin 전환

--- backend/project2/src/main/java/com/team8/project2/domain/curation/tag/dto/TagResDto.kt
+++ backend/project2/src/main/java/com/team8/project2/domain/curation/tag/dto/TagResDto.kt
@@ -1,20 +1,14 @@
-package com.team8.project2.domain.curation.tag.dto;
+package com.team8.project2.domain.curation.tag.dto
 
-import java.util.ArrayList;
-import java.util.List;
+import com.team8.project2.domain.curation.tag.entity.Tag
 
-import com.team8.project2.domain.curation.tag.entity.Tag;
-
-import lombok.Getter;
-
-@Getter
-public class TagResDto {
-	List<String> tags;
-
-	public static TagResDto of(List<Tag> topTags) {
-		TagResDto tagResDto = new TagResDto();
-		tagResDto.tags = new ArrayList<>();
-		topTags.forEach(tag -> tagResDto.tags.add(tag.getName()));
-		return tagResDto;
-	}
+data class TagResDto(
+    val tags: List<String>,
+) {
+    companion object {
+        fun of(topTags: List<Tag>): TagResDto =
+            TagResDto(
+                tags = topTags.map { it.name },
+            )
+    }
 }
