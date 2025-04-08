c815e332d6d3109b64ea5ab367cb065e59105368
socra167 <socra167@gmail.com>
2025-04-08 11:05:00 +0900

    refactor: `Tag` Kotlin 전환

--- backend/project2/src/main/java/com/team8/project2/domain/curation/tag/entity/Tag.kt
+++ backend/project2/src/main/java/com/team8/project2/domain/curation/tag/entity/Tag.kt
@@ -1,44 +1,35 @@
-package com.team8.project2.domain.curation.tag.entity;
+package com.team8.project2.domain.curation.tag.entity
 
-import com.team8.project2.domain.curation.curation.entity.CurationTag;
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.util.List;
+import com.team8.project2.domain.curation.curation.entity.CurationTag
+import jakarta.persistence.*
 
 /**
  * 태그(Tag) 엔티티 클래스입니다.
  * 태그는 큐레이션과 연관되어 특정 주제를 나타낼 수 있습니다.
  */
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "tags")
-public class Tag {
-
-    public Tag(String name) {
-        this.name = name;
-    }
-
+class Tag(
     /**
      * 태그의 고유 ID (자동 생성)
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "tagId", nullable = false)
-    private Long id;
-
+    val id: Long? = null,
     /**
      * 태그 이름 (중복 불가, 필수값)
      */
     @Column(unique = true, nullable = false)
-    private String name;
-
+    var name: String,
     /**
      * 태그와 연관된 큐레이션 목록 (1:N 관계)
      */
     @OneToMany(mappedBy = "tag")
-    private List<CurationTag> curationTags;
+    val curationTags: MutableList<CurationTag> = mutableListOf(),
+) {
+    constructor(name: String) : this(
+        id = null,
+        name = name,
+    )
 }
