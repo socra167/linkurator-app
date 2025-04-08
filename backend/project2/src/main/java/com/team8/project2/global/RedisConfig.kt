6d1d332184e5c8a55b895bc35c8ceeeb1bb8daca
socra167 <socra167@gmail.com>
2025-04-08 17:43:49 +0900

    refactor: RedisConfig Kotlin 전환

---
+++ backend/project2/src/main/java/com/team8/project2/global/RedisConfig.kt
@@ -0,0 +1,36 @@
+package com.team8.project2.global
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig {
+    @Value("\${spring.data.redis.host}")
+    var redisHost: String? = null
+
+    @Value("\${spring.data.redis.port}")
+    var redisPort = 0
+
+    @Value("\${spring.data.redis.timeout}")
+    var redisTimeout: String? = null
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        return LettuceConnectionFactory(redisHost, redisPort)
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, Any> {
+        val template = RedisTemplate<String, Any>()
+        template.connectionFactory = redisConnectionFactory()
+        template.keySerializer = StringRedisSerializer()
+        template.valueSerializer = StringRedisSerializer()
+        return template
+    }
+}
+
