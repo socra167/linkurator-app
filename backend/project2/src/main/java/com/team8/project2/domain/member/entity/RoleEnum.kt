862735d4ddb90e7f8e7e06459124c59afca5754c
taesu0608 <taesu0608@gmail.com>
2025-04-08 11:57:13 +0900

    refactor: RoleEnum 코틀린 전환

--- backend/project2/src/main/java/com/team8/project2/domain/member/entity/RoleEnum.kt
+++ backend/project2/src/main/java/com/team8/project2/domain/member/entity/RoleEnum.kt
@@ -1,5 +1,7 @@
 package com.team8.project2.domain.member.entity;
 
-public enum RoleEnum {
-    ADMIN, USER, MEMBER
+enum class RoleEnum(val description: String) {
+    ADMIN("관리자"),
+    USER("일반 사용자"),
+    MEMBER("회원")
 }
