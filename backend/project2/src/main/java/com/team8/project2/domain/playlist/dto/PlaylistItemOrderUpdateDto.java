5113b0774250ec43c5031cb8df8f3f13d1491a2a
yunjuKimm <yunju.tech@gmail.com>
2025-04-08 10:32:15 +0900

    feat: PlaylistItemOrderUpdateDto Kotlin 변환, Java deprecated 처리

--- backend/project2/src/main/java/com/team8/project2/domain/playlist/dto/PlaylistItemOrderUpdateDto.java
+++ backend/project2/src/main/java/com/team8/project2/domain/playlist/dto/PlaylistItemOrderUpdateDto.java
@@ -6,6 +6,12 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+/**
+ * 플레이리스트 아이템 순서 변경 요청용 DTO입니다.
+ * 메인 아이템 ID와 큐레이션 그룹일 경우, 하위 아이템 ID 목록을 포함합니다.
+ * @deprecated Kotlin으로 마이그레이션됨. PlaylistItemOrderUpdateDto.kt
+ */
+@Deprecated
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
