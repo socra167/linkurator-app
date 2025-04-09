0a1ff2dc732de018231d653cafcdce7bafd7ee6b
taesu0608 <taesu0608@gmail.com>
2025-04-09 16:40:32 +0900

    refactor: FollowResDto 코틀린 전환

--- backend/project2/src/main/java/com/team8/project2/domain/member/dto/FollowResDto.kt
+++ backend/project2/src/main/java/com/team8/project2/domain/member/dto/FollowResDto.kt
@@ -1,22 +1,21 @@
-package com.team8.project2.domain.member.dto;
+package com.team8.project2.domain.member.dto
 
-import com.team8.project2.domain.member.entity.Follow;
-import lombok.Getter;
+import com.team8.project2.domain.member.entity.Follow
+import java.time.LocalDateTime
 
-import java.time.LocalDateTime;
-
-@Getter
-public class FollowResDto {
-
-	private String followee;
-	private String profileImage;
-	private LocalDateTime followedAt;
-
-	public static FollowResDto fromEntity(Follow follow) {
-		FollowResDto followResDto = new FollowResDto();
-		followResDto.followee = follow.getFolloweeName();
-		followResDto.profileImage = follow.getFolloweeProfileImage();
-		followResDto.followedAt = follow.getFollowedAt();
-		return followResDto;
-	}
+class FollowResDto(
+    val followee: String?,
+    val profileImage: String?,
+    val followedAt: LocalDateTime?
+) {
+    companion object {
+        @JvmStatic
+        fun fromEntity(follow: Follow): FollowResDto {
+            return FollowResDto(
+                follow.followeeName,
+                follow.followeeProfileImage,
+                follow.followedAt
+            )
+        }
+    }
 }
