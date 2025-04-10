f6e054d82a9b7c216d8a636bab66dd69865f9e2f
socra167 <socra167@gmail.com>
2025-04-10 10:09:17 +0900

    refactor: `S3Uploader` 파일명 생성 분리

--- backend/project2/src/main/java/com/team8/project2/domain/image/service/S3Uploader.kt
+++ backend/project2/src/main/java/com/team8/project2/domain/image/service/S3Uploader.kt
@@ -1,6 +1,5 @@
 package com.team8.project2.domain.image.service
 
-import lombok.RequiredArgsConstructor
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -10,10 +9,9 @@ import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import java.io.IOException
-import java.util.*
+import java.util.UUID
 
 @Service
-@RequiredArgsConstructor
 class S3Uploader(
     private val s3Client: S3Client,
     @Value("\${spring.cloud.aws.s3.bucket}")
@@ -32,19 +30,23 @@ class S3Uploader(
     @Transactional
     @Throws(IOException::class)
     fun uploadFile(file: MultipartFile): String {
-        val fileName = UUID.randomUUID().toString() + "_" + file.originalFilename
+        val fileName = generateUniqueFileName(file.originalFilename)
 
-        val putObjectRequest = PutObjectRequest.builder()
-            .bucket(bucketName)
-            .key(fileName)
-            .contentType(file.contentType)
-            .build()
-
-        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.inputStream, file.size))
+        s3Client.putObject(
+            PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(file.contentType)
+                .build(),
+            RequestBody.fromInputStream(file.inputStream, file.size)
+        )
 
         return fileName
     }
 
     val baseUrl: String
         get() = "https://$bucketName.s3.amazonaws.com/"
+
+    private fun generateUniqueFileName(originalName: String?): String =
+        "${UUID.randomUUID()}_${originalName ?: "unknown"}"
 }
