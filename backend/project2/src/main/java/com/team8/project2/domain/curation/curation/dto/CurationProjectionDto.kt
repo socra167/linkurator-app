850ecb0480f5ec5243be92bcb9595fdbcbd0ca30
socra167 <socra167@gmail.com>
2025-04-11 12:38:11 +0900

    feat: QueryDSL의 @QueryProjection을 사용하기 위해 별도 DTO 생성

---
+++ backend/project2/src/main/java/com/team8/project2/domain/curation/curation/dto/CurationProjectionDto.kt
@@ -0,0 +1,16 @@
+package com.team8.project2.domain.curation.curation.dto
+
+import com.querydsl.core.annotations.QueryProjection
+import java.time.LocalDateTime
+
+data class CurationProjectionDto @QueryProjection constructor(
+    val id: Long,
+    val title: String,
+    val content: String,
+    val viewCount: Long,
+    val authorName: String,
+    val memberImgUrl: String?,
+    val createdAt: LocalDateTime?,
+    val modifiedAt: LocalDateTime?,
+    val commentCount: Int
+)
